3e6eee5d994e469fef2711041e65bd3c6c107c7c
Zhuoxin Qian <zqzq202411@outlook.com>
2024-04-10 21:40:02 +0000

    edited hr_joins base table

--- models/base/google_drive/base_google_drive__hr_joins.sql
+++ models/base/google_drive/base_google_drive__hr_joins.sql
@@ -1,1 +1,12 @@
-SELECT 
+SELECT _FILE,
+       _LINE,
+       _MODIFIED AS _MODIFIED_TS,
+       CAST(EMPLOYEE_ID AS STRING) AS EMPLOYEE_ID,
+       CAST(REPLACE(HIRE_DATE, 'day  ', '') AS DATE) AS HIRE_DATE,
+       NAME,
+       CITY,
+       ADDRESS,
+       TITLE,
+       ANNUAL_SALARY,
+       _FIVETRAN_SYNCED AS _FIVETRAN_SYNCED_TS
+FROM {{ source('google_drive', 'hr_joins') }}
